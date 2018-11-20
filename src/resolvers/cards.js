42be1e624ce3b5497fa9bc708991047957fe65e4
Jesse Florig <jesseflorig@gmail.com>
2018-11-20 11:34:26 -0500

    added fuzzy search

--- src/resolvers/cards.js
+++ src/resolvers/cards.js
@@ -1,5 +1,19 @@
-const getCards = ({ keywords, name, packs, side, types }) => {
-  return [{ title: "Card Name" }]
+import Fuse from "fuse.js"
+import Cards from "netrunner-json"
+
+const searchCfg = {
+  distance: 100,
+  keys: ["title"],
+  location: 0,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  threshold: 0.6
+}
+
+const getCards = ({ queryString }) => {
+  const fuse = new Fuse(Cards, searchCfg)
+  const results = fuse.search(queryString)
+  return results
 }
 
 export { getCards }
