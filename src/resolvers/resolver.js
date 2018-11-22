b7b579b3e4b80806ecca34ee574e0d6bf769a2b0
Jesse Florig <jesseflorig@gmail.com>
2018-11-22 02:07:56 -0500

    convert card to interface

--- src/resolvers/resolver.js
+++ src/resolvers/resolver.js
@@ -1,6 +1,26 @@
 import { getCards, getDistinct } from "./cards"
 
 const resolverMap = {
+  Card: {
+    __resolveType(card, context, info) {
+      switch (card.type) {
+        case "AGENDA":
+          return "Agenda"
+        case "ASSET":
+          return "Asset"
+        case "ICE":
+          return "Ice"
+        case "IDENTITY":
+          return "Identity"
+        case "OPERATION":
+          return "Operation"
+        case "UPGRADE":
+          return "Upgrade"
+      }
+
+      return null
+    }
+  },
   Query: {
     searchCards(obj, args, context, info) {
       return getCards(args)
