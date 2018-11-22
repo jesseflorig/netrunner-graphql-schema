4b2099114f187b3c81eb84eda02a964b297ffad2
Jesse Florig <jesseflorig@gmail.com>
2018-11-22 00:32:16 -0500

    extended search

--- src/resolvers/cards.js
+++ src/resolvers/cards.js
@@ -2,23 +2,23 @@ import Fuse from "fuse.js"
 import _ from "lodash"
 import { Cards } from "netrunner-json"
 
-const searchCfg = {
-  distance: 100,
-  keys: ["title"],
-  location: 0,
-  maxPatternLength: 32,
-  minMatchCharLength: 1,
-  shouldSort: true,
-  threshold: 0.6,
-  tokenize: true
-}
-
-const getCards = ({ filter, fuzzyString, reject }) => {
+const getCards = ({ filter, fuzzyFields, fuzzySearch, orderBy, reject }) => {
+  const searchCfg = {
+    distance: 100,
+    keys: fuzzyFields || ["title"],
+    location: 0,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    shouldSort: true,
+    threshold: 0.6,
+    tokenize: true
+  }
   const filterCards = filter ? _.filter(Cards, filter) : Cards
   const rejectCards = reject ? _.reject(filterCards, reject) : filterCards
-  return fuzzyString
-    ? new Fuse(rejectCards, searchCfg).search(fuzzyString)
+  const searchCards = fuzzySearch
+    ? new Fuse(rejectCards, searchCfg).search(fuzzySearch)
     : rejectCards
+  return _.orderBy(searchCards, orderBy || ["title"])
 }
 
 const getDistinct = ({ field }) => {
